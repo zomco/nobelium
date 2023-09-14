9f6f6617c6a941ac9c5177a996b7b229e2293099
zomco <zomcoo@gmail.com>
2023-09-14 14:17:17 +0800

    update favicon

--- blog.config.js
+++ blog.config.js
@@ -34,7 +34,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-13729Y98N3' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-VY1YFY0ZK3' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
